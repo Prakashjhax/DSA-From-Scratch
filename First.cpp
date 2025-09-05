6267836dca8ec5ae93ea4d27aa4fdc5673d6714c
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-09-05 23:19:40 +0530

    Basics

--- First.cpp
+++ First.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello world I am Prakash Kumar Jha  ";
+    // variable declare 4 bytes
+    int a =10, b=20, c=30, d, e=11;
+    cout<<a+c;
+
+
+    char c='a';
+    cout<<c;
+
+    float f=2.31;
+    cout<<f;
+
+
+    bool b = true;
+    cout<<b;
+
+
 }
