51a20c96d7dfc858eb53d1a5732cda52b83f73e6
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-09-06 03:29:09 +0530

    Second

---
+++ second.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout<<"Enter your first Number: ";
+    cin>>a;
+    cout<<"Enter Your Second Number: ";
+    cin>>b;
+
+
+    cout<<"Sum of Two numbers: "<<a+b;
+} 
+
+// TypeCasting 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a=66454;
+    char c= 'd';
+    // a=c;
+    c=a;
+    cout<<c;
+}
+
+
