acb1c24dc09300a0a1c2762a740009bffa92d51a
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-09-13 15:23:13 +0530

    Pattern Printing part 2

--- pat_print_2.cpp
+++ pat_print_2.cpp
@@ -1,80 +1,133 @@
-// print star in triangle form 
+// // print star in triangle form 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row,col;
+//     for (row=1; row<=5; row=row+1){
+//         for(col=1; col<=row; col=col+1){
+//             cout<<"*"<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// // print 1,2,3,4,5 in a triangle form 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row, col;
+//     for(row=1; row<=6; row=row+1)
+//     {
+//         for(col=1; col<=row; col=col+1){
+//             cout<<col<<" ";
+//         }
+
+//         cout<<endl;
+//     }
+// }
+
+// // print 1, 22, 33,444,555 in triangle form 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row,col;
+//     for(row=1; row<=5; row=row+1)
+//     {
+//         for(col=1; col<=row; col=col+1)
+//         {
+//             cout<<row<<" ";
+
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row,col;
+//     for(row=1; row<=5; row=row+1)
+//     {
+//         for(col=1; col<=row; col=col+1)
+//         {
+//             char name ='a' +row-1;
+//             cout<<name<<" ";
+
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// // print reverse triangle star printing
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row, col;
+//     for(row=1; row<=5; row=row+1)
+//     {
+//         for (col=1; col<=5-row+1; col=col+1){
+//             cout<<" * ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+// // // 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row, col;
+//     for(row=1; row<=5; row=row+1)
+//     {
+//         for (col=1; col<=5-row+1; col=col+1){
+//             cout<<col<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+// // 
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row, col,n;
+//     cout<<"Enter your number : ";
+//     cin>>n;
+//     for(row=1; row<=n; row= row+1)
+//     {
+//         for(col=1; col<=n-row; col=col+1)
+//         cout<<"  ";
+//     for(col=1; col<=row; col= col+1)
+//     cout<<row<<" ";
+    
+//     cout<<endl;
+
+//     }
+// }
+
 
 #include<iostream>
 using namespace std;
 int main(){
-    int row,col;
-    for (row=1; row<=5; row=row+1){
-        for(col=1; col<=row; col=col+1){
-            cout<<"*"<<" ";
-        }
-        cout<<endl;
-    }
-}
-
-// print 1,2,3,4,5 in a triangle form 
-
-#include<iostream>
-using namespace std;
-int main(){
-    int row, col;
-    for(row=1; row<=6; row=row+1)
-    {
-        for(col=1; col<=row; col=col+1){
-            cout<<col<<" ";
-        }
-
-        cout<<endl;
-    }
-}
-
-// print 1, 22, 33,444,555 in triangle form 
-
-#include<iostream>
-using namespace std;
-int main(){
-    int row,col;
+    int row , col;
     for(row=1; row<=5; row=row+1)
     {
-        for(col=1; col<=row; col=col+1)
-        {
-            cout<<row<<" ";
+        for (col=1; col<=5-row; col= col+1)
+        cout<<"  ";
 
-        }
+        for (col=1; col<=row; col=col+1)
+        cout<<col<<" ";
+        
         cout<<endl;
     }
 }
-
-
-#include<iostream>
-using namespace std;
-int main(){
-    int row,col;
-    for(row=1; row<=5; row=row+1)
-    {
-        for(col=1; col<=row; col=col+1)
-        {
-            char name ='a' +row-1;
-            cout<<name<<" ";
-
-        }
-        cout<<endl;
-    }
-}
-
-// print reverse triangle star printing
-
-#include<iostream>
-using namespace std;
-int main(){
-    int row, col;
-    for(row=1; row<=5; row=row+1)
-    {
-        for (col=1; col<=5-row+1; col=col+1){
-            cout<<" * ";
-        }
-        cout<<endl;
-    }
-}
-
-
