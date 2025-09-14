d32e62a38228b8f2d152c56459b1736710f5199f
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-09-15 02:15:53 +0530

    Operators

---
+++ operators.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<13.7/2;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<13.2*4;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+
+    cout<<a++;
+    cout<<endl<<a;
+}
+
+// same for all the operator , it will work oky 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b: ";
+    cin>>b;
+
+
+    if(a==b)
+    cout<<"Yes";
+    else
+    cout<<"no";
+
+}
