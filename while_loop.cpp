ea995ee6cc828f95ff10f44dcb37706b7c3161d1
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-09-16 18:41:23 +0530

    While_loop

--- while_loop.cpp
+++ while_loop.cpp
@@ -33,3 +33,89 @@ int main()
 
     }while(i<=n);
 }
+
+// break statement 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter your number: ";
+    cin>>n;
+    for (int i=1;i<=n;i++){
+        if(i==5){
+            break;
+        }
+        cout<<i<<endl;
+    }
+}
+
+
+// continue statement
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter your number: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(i==5){
+            continue;
+        }
+        cout<<i<<endl;
+    }
+}
+
+
+// switch case 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter your number: ";
+    cin>>n;
+    switch(n){
+        case 1:
+            cout<<"Monday"<<endl;
+            break;
+        case 2:
+            cout<<"Tuesday"<<endl;
+            break;
+        case 3:
+            cout<<"Wednesday"<<endl;
+            break;
+        case 4:
+            cout<<"Thursday"<<endl;
+            break;
+        case 5:
+            cout<<"Friday"<<endl;
+            break;
+        case 6:
+            cout<<"Saturday"<<endl;
+            break;
+        case 7:
+            cout<<"Sunday"<<endl;
+            break;
+        default:
+            cout<<"Invalid Input"<<endl;
+
+    }
+}
+
+
+
+// scope of variable 
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    {
+        int a=20;
+        cout<<a<<endl;
+    }
+    cout<<a<<endl;
+}
+
+
+
