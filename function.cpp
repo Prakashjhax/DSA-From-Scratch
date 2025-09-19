49ae19f5b3e0ee047eb038ef9d4337f4bc0a84e5
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-09-19 19:01:21 +0530

    Function

---
+++ function.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int sum(int m, int n){
+    int ans = m+n;
+    return ans;
+
+}
+
+int main(){
+    int a,b;
+    cout<<"Enter two numbers :";
+    cin>>a>>b;
+    
+    cout<<sum(a,b);
+    cout<<endl;
+}
