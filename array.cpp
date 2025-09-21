daa78db9061242183c5ba301ee72ae8b08b0f1e9
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-09-22 02:05:43 +0530

    Array

---
+++ array.cpp
@@ -0,0 +1,33 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[5]={1,2,3,4,5};
+//     for (int i=0; i<5; i++)
+//         cout<<arr[i]<<" ";
+//         return 0;
+    
+
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int size=5;
+
+//     cin>>size;
+
+//     int arr[1000];
+
+//     for(int i=0; i<size; i++)
+
+//     cin>>arr[i];
+
+//     for(int i=0; i<size; i++)
+
+//     cout<<arr[i]<<" ";
+
+//     return 0;
+
+// }
