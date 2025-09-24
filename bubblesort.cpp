c50b27334c5a9fc3e9d8485486759b20dbf68398
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-09-25 02:48:43 +0530

    Bubble Sort

---
+++ bubblesort.cpp
@@ -0,0 +1,36 @@
+// bubble sort code part 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[1000];
+    int n;
+    cout<<"Enter the number of elements : ";
+    cin>>n;
+    cout<<"Enteer the elements : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=n-2; i>=0; i--)
+    // inner loop
+
+    {
+        bool swapped = 0;
+        for(int j=0; j<=i; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swapped = 1;
+                swap(arr[j], arr[j+1]);
+            }
+
+        }
+        if(swapped == 0)
+        break;
+    }
+
+    for(int i=0; i<n; i++)
+    cout<<arr[i]<<" ";
+    return 0;
+}
