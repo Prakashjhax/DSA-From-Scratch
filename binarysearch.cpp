aec79b8e90c02dd090c4fd99461d7f1250cf9726
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-09-28 16:11:01 +0530

    Binary Search

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -39,3 +39,73 @@ int main(){
     return 0;
 
 }
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findFirst(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1, ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1; // move left
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    int findLast(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1, ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1; // move right
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return {first, last};
+    }
+};
+
+int main() {
+    Solution sol;
+
+    int n, target;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter sorted array elements: ";
+    for (int i = 0; i < n; i++) cin >> nums[i];
+
+    cout << "Enter target: ";
+    cin >> target;
+
+    vector<int> result = sol.searchRange(nums, target);
+    cout << "Output: [" << result[0] << ", " << result[1] << "]" << endl;
+
+    return 0;
+}
