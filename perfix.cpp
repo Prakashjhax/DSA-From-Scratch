e7fcda53e62a3cf0fa9a436b1948912f862b6ec7
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-10-21 20:00:50 +0530

    Prefix.Cpp

--- perfix.cpp
+++ perfix.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-using namespace std;
-int main()
-{
-    int n;
-    cout<<"Enter size of array: ;";
-    cin>>n;
-    vector<int>arr(n);
-    cout<<"Enter the element in array: ";
-    for(int i=0;i<n;i++)
-    cin>>v[i];
-}
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout<<"Enter size of array: ;";
+//     cin>>n;
+//     vector<int>arr(n);
+//     cout<<"Enter the element in array: ";
+//     for(int i=0;i<n;i++)
+//     cin>>v[i];
+// }
