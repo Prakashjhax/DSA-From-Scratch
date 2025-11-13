8eac84f131afb57fc4d33f759958b2ac211097a7
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-11-14 00:25:50 +0530

    Vector 2 D

--- vector2d.cpp
+++ vector2d.cpp
@@ -1,14 +1,37 @@
 #include<iostream>
-#incclude <vector>
-#include<algorithm>>
+#include<vector>
+#include<algorithm>
 using namespace std;
 int main()
 {
     // create 2d vector
-    vector<vector<int> >matrix(3, vector<int>(4,0));
+    int n,m;
+    cout <<"Enter rows and cols: ";
+    cin>>n>>m;
+    vector<vector<int> >matrix(n, vector<int>(m,1));
     
     for(int i=0; i<3; i++)
     for (int j=0; j<4; j++)
     cout<<matrix[i][j]<<" ";
 
+    cout<<"Rows: "<<matrix.size();
+    cout<<endl;
+    cout<<"Cols: "<<matrix[0].size();
+
+
+    for(int i=0; i<n; i++)
+    for(int j=0; j<m; j++)
+    cin>>matrix[i][j];
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        cout<<matrix[i][j]<<" ";
+        cout<<endl;
+
+    }     
+    
+
 }
+
+
