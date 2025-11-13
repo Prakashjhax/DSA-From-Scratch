61ca0e343006284cbda947d5a086ea3359a54044
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-11-13 22:12:22 +0530

    vector 2D

---
+++ vector2d.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#incclude <vector>
+#include<algorithm>>
+using namespace std;
+int main()
+{
+    // create 2d vector
+    vector<vector<int> >matrix(3, vector<int>(4,0));
+    
+    for(int i=0; i<3; i++)
+    for (int j=0; j<4; j++)
+    cout<<matrix[i][j]<<" ";
+
+}
